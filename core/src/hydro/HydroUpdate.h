4167f2e0fd8c4079f7265962e523165eba8be063
Arnaud Durocher <arnaud.durocher@cea.fr>
2024-04-15 13:30:20 +0200

    Refactor HydroUpdate_euler_no_patch to extract FiniteVolumePolicy

--- core/src/hydro/HydroUpdate.h
+++ core/src/hydro/HydroUpdate.h
@@ -14,6 +14,9 @@ template<typename State> class HydroUpdate_euler;
 template<typename State> class HydroUpdate_RK2;
 template<typename State> class HydroUpdate_euler_no_patch;
 
+template<int ndim> 
+class FiniteVolumePolicy_hydro;
+
 } //namespace dyablo 
 
 
@@ -32,7 +35,7 @@ inline bool dyablo::HydroUpdateFactory::init()
   DECLARE_REGISTERED(dyablo::HydroUpdate_RK2<dyablo::HydroState>);
   DECLARE_REGISTERED(dyablo::HydroUpdate_RK2<dyablo::MHDState>);
   DECLARE_REGISTERED(dyablo::HydroUpdate_RK2<dyablo::GLMMHDState>);
-  DECLARE_REGISTERED(dyablo::HydroUpdate_euler_no_patch<dyablo::HydroState>);
+  DECLARE_REGISTERED(dyablo::HydroUpdate_euler_no_patch<dyablo::FiniteVolumePolicy_hydro<3>>);
 
   return true;
 }
--- core/src/hydro/HydroUpdate_euler_no_patch.cpp
+++ core/src/hydro/HydroUpdate_euler_no_patch.cpp
@@ -3,6 +3,7 @@
 #include "HydroUpdate_utils.h"
 
 #include "boundary_conditions/BoundaryConditions.h"
+#include "mpi/GhostCommunicator_partial_blocks.h"
 
 namespace dyablo {
 namespace{
@@ -77,24 +78,174 @@ void apply_gravity_correction( const FieldAccessor& Uin,
 
 namespace dyablo {
 
+template< int ndim_ >
+class FiniteVolumePolicy_hydro 
+{
+public:
+  constexpr static int ndim = ndim_;
+  using PrimState = PrimHydroState;
+  using ConsState = ConsHydroState;
+  using CellIndex = ForeachCell::CellIndex;
+
+  struct Params{
+    real_t gamma0;
+    RiemannParams rparams;
+    BoundaryConditions boundary_conditions;
+  };
+
+  FiniteVolumePolicy_hydro( ConfigMap& configMap )
+  : params{
+      .gamma0 = configMap.getValue<real_t>("hydro","gamma0", 1.4),
+      .rparams = configMap,
+      .boundary_conditions = configMap
+    }
+  {}
+
+  FieldAccessor getUin( UserData& U ) const
+  {
+    return U.getAccessor( ConsState::getFieldsInfo() );
+  }
+
+  FieldAccessor getUout( UserData& U ) const
+  {
+    auto fields_info_next = ConsState::getFieldsInfo();
+    for( auto& p : fields_info_next )
+      p.name += "_next";
+    return U.getAccessor( fields_info_next );
+  }
+
+  template < typename Array_t >
+  ConsState getConsState( const Array_t& U, const CellIndex& iCell ) const
+  {
+    ConsState u;
+    getConservativeState<ndim>(U, iCell, u);
+    return u;
+  }
+
+  template < typename Array_t >
+  void setConsState( const Array_t& U, const CellIndex& iCell, const ConsState& u ) const
+  {
+    setConservativeState<ndim>(U, iCell, u);
+  }
+
+  template < typename Array_t >
+  void atomic_addConsState( const Array_t& U, const CellIndex& iCell, const ConsState& u ) const
+  {
+    Kokkos::atomic_add(&U.at(iCell, ConsState::Irho), u.rho);
+    Kokkos::atomic_add(&U.at(iCell, ConsState::Ie_tot), u.e_tot);
+    Kokkos::atomic_add(&U.at(iCell, ConsState::Irho_vx), u.rho_u);
+    Kokkos::atomic_add(&U.at(iCell, ConsState::Irho_vy), u.rho_v);
+    if (ndim == 3)
+      Kokkos::atomic_add(&U.at(iCell, ConsState::Irho_vz), u.rho_w);
+  }
+
+  template < typename Array_t >
+  PrimState getPrimState( const Array_t& Q, const CellIndex& iCell ) const
+  {
+    PrimState q;
+    getPrimitiveState<ndim>(Q, iCell, q);
+    return q;
+  }
+
+  template < typename Array_t >
+  void setPrimState( const Array_t& Q, const CellIndex& iCell, const PrimState& q ) const
+  {
+    setPrimitiveState<ndim>(Q, iCell, q);
+  }
+
+  PrimState consToPrim( const ConsState& u ) const
+  {
+    return dyablo::consToPrim<ndim>(u, params.gamma0);
+  }
+
+  ConsState primToCons( const PrimState& q ) const
+  {
+    return dyablo::primToCons<ndim>(q, params.gamma0);
+  }
+
+  ConsState riemann_solver( PrimState qL, PrimState qR, ComponentIndex3D dir ) const
+  {
+    qL = swapComponents(qL, dir);
+    qR = swapComponents(qR, dir);
+    ConsState flux = riemann_hydro(qL, qR, params.rparams);
+    flux = swapComponents(flux, dir);
+    return flux;
+  }
+
+  PrimState compute_slope( PrimState qL, PrimState qC, PrimState qR, real_t dL, real_t dR) const
+  {
+    auto dqp = (qR - qC) / dR;
+    auto dqm = (qC - qL) / dL;
+
+    PrimState slope{};
+    state_foreach_var([](real_t& res, real_t dvp, real_t dvm) {
+      if (dvp * dvm <= 0.0)
+        res = 0.0;
+      else
+        res = fabs(dvp) > fabs(dvm) ? dvm : dvp;
+    }, slope, dqp, dqm);
+
+    return slope;
+  }
+
+  template < typename Array_t >
+  ConsState getBoundaryValue( const Array_t &U, const CellIndex &iCell_boundary, const CellMetaData &metadata) const
+  {
+    return params.boundary_conditions.template getBoundaryValue<ndim, HydroState>(U, iCell_boundary, metadata );
+  }
+
+private:
+  const Params params;
+};
+
+template< typename Policy, typename Array_t >
+void clean_negative_primitive_values(const Policy& policy, const ForeachCell& foreach_cell, const Array_t& U, double smallr, double smallp)
+{
+  using PrimState = typename Policy::PrimState;
+  using ConsState = typename Policy::ConsState;
+
+  int negative_p_count=0;
+  int negative_rho_count=0;
+
+  foreach_cell.reduce_cell( "clean_negative_values", U.getShape(),
+    KOKKOS_LAMBDA(  const ForeachCell::CellIndex& iCell, 
+                    int& negative_p_count, 
+                    int& negative_rho_count )
+  {
+    ConsState u = policy.getConsState(U, iCell);
+    PrimState q = policy.consToPrim(u);
+    if( q.rho < 0.0 || q.p < 0.0 )
+    {
+      if (q.rho < 0.0) {
+        negative_rho_count++;
+        q.rho = smallr;
+      }
+      if (q.p < 0.0) {
+        negative_p_count++;
+        q.p   = smallp;
+      }
+      ConsState u = policy.primToCons(q);
+      policy.setConsState(U, iCell, u);
+    }    
+  }, negative_p_count, negative_rho_count);
+
+  if( negative_rho_count > 0 )
+    printf("WARNING ! Negative density detected (x%d) !!!\n", negative_rho_count);
+  if( negative_p_count > 0 )
+    printf("WARNING ! Negative pressure detected (x%d) !!!\n", negative_p_count);
+
+}
+
 /**
  * @brief Euler update algorithm
  * 
  * @tparam State the type of state to treat
  */
-template<typename State_>
+template<typename Policy>
 class HydroUpdate_euler_no_patch : public HydroUpdate {
 public:
-  using State     = State_;
-  using PrimState = typename State::PrimState;
-  using ConsState = typename State::ConsState;
-
-private:
-  struct ValueSlope {
-    PrimState q;
-    PrimState slope;
-    real_t size;
-  };
+  using PrimState = typename Policy::PrimState;
+  using ConsState = typename Policy::ConsState;
 
 public:
   HydroUpdate_euler_no_patch(
@@ -103,10 +254,11 @@ public:
           Timers& timers) 
   : foreach_cell(foreach_cell),
     timers(timers),
-    params(configMap),
-    bc_manager(configMap),
+    policy(configMap),
     gravity_type(configMap.getValue<GravityType>("gravity", "gravity_type", GRAVITY_NONE)),
-    well_balanced(configMap.getValue<bool>("hydro", "well_balanced", false))
+    well_balanced(configMap.getValue<bool>("hydro", "well_balanced", false)),
+    smallr( configMap.getValue<real_t>("hydro","smallr", 1e-10) ),
+    smallp( configMap.getValue<real_t>("hydro","smallp", 1e-10) )
   {
     if (gravity_type & GRAVITY_CONSTANT) {
       gx = configMap.getValue<real_t>("gravity", "gx", 0.0);
@@ -123,153 +275,92 @@ public:
   /**
    * @brief Solves hydro for one step using the euler method
    * 
-   * @param Uin the input global array
-   * @param Uout the output global array
-   * @param dt the timestep
+   * @param U the input/output global array
+   * @param scalar_data input scalar data
    */
-  void update( UserData& U, ScalarSimulationData& scalar_data) 
+  void update( UserData& U, ScalarSimulationData& scalar_data)
   {
     real_t dt = scalar_data.get<real_t>("dt");
-    uint32_t ndim = foreach_cell.getDim();
-    if (ndim == 2)
-      update_aux<2>(U, dt);
-    else if (ndim == 3)
-      update_aux<3>(U, dt);  
-    else
-      DYABLO_ASSERT_HOST_RELEASE(false, "invalid ndim = " << ndim);
-  }
+    constexpr int ndim = Policy::ndim;
 
-  template< 
-    int ndim >
-  void update_aux( UserData& U, real_t dt) 
-  {
-
-    const RiemannParams& params = this->params; 
-    auto bc_manager = this->bc_manager;
+    const Policy& policy = this->policy; 
     Timers& timers = this->timers; 
     ForeachCell& foreach_cell = this->foreach_cell;
 
-    auto fields_info = ConsState::getFieldsInfo();
-    FieldAccessor Uin = U.getAccessor( fields_info );
-    //FieldAccessor Uin_g;
-    //if( gravity_use_field )
-    //  Uin_g = U.getAccessor( {{"gx",IGX}, {"gy",IGY}, {"gz",IGZ}} );
-    auto fields_info_next = fields_info;
-    for( auto& p : fields_info_next )
-      p.name += "_next";
-    FieldAccessor Uout = U.getAccessor( fields_info_next );
+    FieldAccessor Uin = policy.getUin(U);
+    FieldAccessor Uout = policy.getUout(U);
     
     timers.get("HydroUpdate_euler").start();
 
     ForeachCell::CellMetaData cellmetadata = foreach_cell.getCellMetaData();
 
-    // Initializing output array
+    // Initializing output array 
+    // TODO : remove this and copy Uin->Uout in timeloop or field creation logic
     foreach_cell.foreach_cell( "HydroUpdate_euler_no_patch::init",
       Uout.getShape(),
-      CELL_LAMBDA(const CellIndex &iCell) {
-        ConsState uC;
-        getConservativeState<ndim>(Uin, iCell, uC);
-        setConservativeState<ndim>(Uout, iCell, uC);
-      });
+      CELL_LAMBDA(const CellIndex &iCell) 
+    {
+      ConsState uC = policy.getConsState(Uin, iCell);
+      policy.setConsState(Uout, iCell, uC);
+    });
 
     // Setting the ghosts to 0 to accumulate fluxes
     foreach_cell.foreach_ghost_cell( "HydroUpdate_euler_no_patch::resetting_ghosts",
       Uout.getShape(),
-      CELL_LAMBDA(const CellIndex &iCell) {
-        ConsState empty_state{};
-        setConservativeState<ndim>(Uout, iCell, empty_state);
-      });
+      CELL_LAMBDA(const CellIndex &iCell) 
+    {
+      ConsState empty_state{};
+      policy.setConsState(Uout, iCell, empty_state);
+    });
 
     // Iterate over cells
     foreach_cell.foreach_cell( "HydroUpdate_euler_no_patch::update",
       Uout.getShape(),
       CELL_LAMBDA(const CellIndex &iCell)
     {
-      // Returns a value at a position and its associated slope
-      auto get_value_and_slope = [&](const CellIndex &iCell, ComponentIndex3D dir) {
-        // Getting stencil
-        offset_t off_m{}, off_p{};
-        off_m[dir] = -1;
-        off_p[dir] =  1;
+      // Return Slope at position iCell
+      auto get_slope = [&](const CellIndex &iCell, ComponentIndex3D dir) {        
 
-        CellIndex iCell_m = iCell.getNeighbor_ghost(off_m, Uout.getShape());
-        CellIndex iCell_p = iCell.getNeighbor_ghost(off_p, Uout.getShape());
+        auto get_neighbor_prim_value = [&]( const CellIndex& iCell_n, const CellIndex::offset_t& off )
+        {
+          ConsState u {};
+          // Getting left value
+          int level_diff = iCell_n.level_diff();
+          if (iCell_n.is_boundary())
+            u = policy.getBoundaryValue(Uin, iCell_n, cellmetadata);
+          else if (level_diff < 0) {
+            int subcell_count = 
+            foreach_smaller_neighbor<ndim>(iCell_n, off, Uin.getShape(),
+              [&](const CellIndex& iCell_neigh) {
+                ConsState uloc = policy.getConsState(Uin, iCell_neigh);
+                u += uloc;
+              });
+            u /= subcell_count;
+          }
+          else
+            u = policy.getConsState(Uin, iCell_n);
 
-        // Reading conservative states
-        ConsState uL{}, uC{}, uR{};
+          return policy.consToPrim(u);
+        };
 
-        if (iCell.is_boundary())
-          uC = bc_manager.template getBoundaryValue<ndim, State>(Uin, iCell, cellmetadata);
-        else
-          getConservativeState<ndim>(Uin, iCell, uC);
-
-        // Getting left value
-        int Ldiff = iCell_m.level_diff();
-        if (iCell_m.is_boundary())
-          uL = bc_manager.template getBoundaryValue<ndim, State>(Uin, iCell_m, cellmetadata);
-        else if (Ldiff < 0) {
-          foreach_smaller_neighbor<ndim>(iCell_m, off_m, Uin.getShape(),
-            [&](const CellIndex& iCell_neigh) {
-              ConsState uloc;
-              getConservativeState<ndim>(Uin, iCell, uloc);
-              uL += uloc;
-            });
-          uL *= (ndim == 2 ? 0.5 : 0.25);
-        }
-        else
-          getConservativeState<ndim>(Uin, iCell_m, uL);
-
-        // Getting right value
-        int Rdiff = iCell_p.level_diff();
-        if (iCell_p.is_boundary())
-          uR = bc_manager.template getBoundaryValue<ndim, State>(Uin, iCell_p, cellmetadata);
-        else if (Rdiff < 0) {
-          foreach_smaller_neighbor<ndim>(iCell_p, off_p, Uin.getShape(),
-            [&](const CellIndex& iCell_neigh) {
-              ConsState uloc;
-              getConservativeState<ndim>(Uin, iCell, uloc);
-              uR += uloc;
-            });
-          uR *= (ndim == 2 ? 0.5 : 0.25);
-        }
-        else
-          getConservativeState<ndim>(Uin, iCell_p, uR);
-
-        // Converting to primitive states
-        const PrimState qL = consToPrim<ndim>(uL, params.gamma0);
-        const PrimState qC = consToPrim<ndim>(uC, params.gamma0);
-        const PrimState qR = consToPrim<ndim>(uR, params.gamma0);      
+        ConsState uC = policy.getConsState(Uin, iCell);
+        const PrimState qC = policy.consToPrim( uC );
+        offset_t off_m{}; off_m[dir] = -1;
+        CellIndex iCell_L = iCell.getNeighbor_ghost(off_m, Uout.getShape());
+        const PrimState qL = get_neighbor_prim_value(iCell_L, off_m);
+        offset_t off_p{}; off_p[dir] =  1;
+        CellIndex iCell_R = iCell.getNeighbor_ghost(off_p, Uout.getShape());
+        const PrimState qR = get_neighbor_prim_value(iCell_R, off_p);    
 
         // Getting the length right and left
-        const real_t sizes[] = {0.75, 1.0, 1.5};
-        const real_t dL = sizes[Ldiff+1];
-        const real_t dR = sizes[Rdiff+1];  
+        constexpr real_t sizes[] = {0.75, 1.0, 1.5};
+        const real_t dL = sizes[iCell_L.level_diff()+1];
+        const real_t dR = sizes[iCell_R.level_diff()+1];  
 
         // Computing minmod slope for the direction
-        PrimState slope{};
-        auto dqp = (qR - qC) / dR;
-        auto dqm = (qC - qL) / dL;
-        state_foreach_var([](real_t& res, real_t dvp, real_t dvm) {
-          if (dvp * dvm <= 0.0)
-            res = 0.0;
-          else
-            res = fabs(dvp) > fabs(dvm) ? dvm : dvp;
-        }, slope, dqp, dqm);
-
-        return ValueSlope{qC, 
-                          slope, 
-                          cellmetadata.getCellSize(iCell)[dir]};
-      }; // get_value_and_slope
-
-      // Adding a state to a view without race conditions
-      auto atomic_add_state = [&](const CellIndex &iC, ConsState flux) {
-        Kokkos::atomic_add(&Uout.at(iC, ConsState::Irho), flux.rho);
-        Kokkos::atomic_add(&Uout.at(iC, ConsState::Ie_tot), flux.e_tot);
-        Kokkos::atomic_add(&Uout.at(iC, ConsState::Irho_vx), flux.rho_u);
-        Kokkos::atomic_add(&Uout.at(iC, ConsState::Irho_vy), flux.rho_v);
-        if (ndim == 3)
-          Kokkos::atomic_add(&Uout.at(iC, ConsState::Irho_vz), flux.rho_w);
-      };
+        PrimState slope = policy.compute_slope( qL, qC, qR, dL, dR);
+        return slope;
+      }; // get_slope
 
 
       auto process_dir = [&](const CellIndex &iCell, ComponentIndex3D dir) {
@@ -282,71 +373,82 @@ public:
         const CellIndex iCell_p = iCell.getNeighbor_ghost(off_p, Uin.getShape());
 
         // Getting centered value and slope
-        ValueSlope qsC = get_value_and_slope(iCell, dir);
+        ConsState uC = policy.getConsState( Uin, iCell );
+        PrimState qC0 = policy.consToPrim(uC);
+        PrimState slope_C = get_slope(iCell, dir);
+        real_t size_C = cellmetadata.getCellSize(iCell)[dir];
 
         constexpr real_t dim_fac = (ndim == 2 ? 0.5 : 0.25);
+
+        ConsState du {};
 
         // Left side
         int Ldiff = iCell_m.level_diff();
         if (Ldiff >= 0) {
-          ValueSlope qsL = get_value_and_slope(iCell_m, dir);
+          ConsState uL = policy.getConsState( Uin, iCell_m );
+          PrimState qL0 = policy.consToPrim(uL);
+          PrimState slope_L = get_slope(iCell_m, dir);
+          real_t size_L = cellmetadata.getCellSize(iCell_m)[dir];
 
           // Reconstructing
-          PrimState qL = qsL.q + 0.5 * qsL.slope;
-          PrimState qC = qsC.q - 0.5 * qsC.slope;
+          PrimState qL = qL0 + 0.5 * slope_L;
+          PrimState qC = qC0 - 0.5 * slope_C;
 
-          //printf("On density slope : %lf %lf; %lf %lf; %lf %lf\n", qsL.q.rho, qsC.q.rho, qsL.slope.rho, qsC.slope.rho, qL.rho, qC.rho); 
           // Solving
-          qL = swapComponents(qL, dir);
-          qC = swapComponents(qC, dir);
-          ConsState flux = riemann_hydro(qL, qC, params);
-          flux = swapComponents(flux, dir);
+          ConsState flux = policy.riemann_solver(qL, qC, dir);
 
-          // And adding flux to output cell, and neighbor if bigger
-          atomic_add_state(iCell, flux * dt / qsC.size);
-          if (Ldiff == 1)
-            atomic_add_state(iCell_m, flux * -dim_fac * dt / qsL.size);
+          // Adding flux to output cell        
+          du += flux * dt / size_C;
+          // Adding flux to the neighbor if it is bigger
+          if (Ldiff == 1) 
+          {
+            ConsState du_n = flux * - dim_fac * dt / size_L;
+            policy.atomic_addConsState(Uout, iCell_m, du_n);
+          }
         } // If smaller we skip
 
         // Right side
         int Rdiff = iCell_p.level_diff();
         if (Rdiff >= 0) {
-          ValueSlope qsR = get_value_and_slope(iCell_p, dir);
+          ConsState uR = policy.getConsState( Uin, iCell_p );
+          PrimState qR0 = policy.consToPrim(uR);
+          PrimState slope_R = get_slope(iCell_p, dir);
+          real_t size_R = cellmetadata.getCellSize(iCell_p)[dir];
 
           // Reconstructing
-          PrimState qC = qsC.q + 0.5 * qsC.slope;
-          PrimState qR = qsR.q - 0.5 * qsR.slope;
+          PrimState qC = qC0 + 0.5 * slope_C;
+          PrimState qR = qR0 - 0.5 * slope_R;
 
           // Solving
-          qC = swapComponents(qC, dir);
-          qR = swapComponents(qR, dir);
-          ConsState flux = riemann_hydro(qC, qR, params);
-          flux = swapComponents(flux, dir);
+          ConsState flux = policy.riemann_solver(qC, qR, dir);
 
-          // And adding flux to output cell, and neighbor if bigger
-          atomic_add_state(iCell, flux * -1.0 * dt / qsC.size);
-
-          // Adding our flux to the neighbor if it is bigger
+          // Adding flux to output cell 
+          du -= flux * dt / size_C;
+          // Adding flux to the neighbor if it is bigger
           if (Rdiff == 1)
-            atomic_add_state(iCell_p, flux * dim_fac * dt / qsR.size);
-        } // If smaller we skip
+          {
+            ConsState du_n = flux * dim_fac * dt / size_R;
+            policy.atomic_addConsState(Uout, iCell_p, du_n);
+          }          
+        } 
+
+        return du;
       };
 
-
-      // Actual work
-      process_dir(iCell, IX);
-      process_dir(iCell, IY);
+      ConsState du{};
+      du += process_dir(iCell, IX);
+      du += process_dir(iCell, IY);
       if (ndim == 3)
-        process_dir(iCell, IZ);
-
+        du += process_dir(iCell, IZ);
+      policy.atomic_addConsState(Uout, iCell, du);
+      
     });
 
     // Reducing the ghosts to accumulate the flux in the data arrays 
-    GhostCommunicator ghost_comm(std::shared_ptr<AMRmesh>(&foreach_cell.get_amr_mesh(), [](AMRmesh*){}));
-    U.reduce_ghosts(ghost_comm);
+    GhostCommunicator_partial_blocks ghost_comm ( foreach_cell.get_amr_mesh().getMesh() );
+    ghost_comm.reduce_ghosts( Uout );
 
-    // Checking that we have not overshot in density and pressure
-    clean_negative_primitive_values<ndim, State>(foreach_cell, Uout, params.gamma0, params.smallr, params.smallp);
+    clean_negative_primitive_values(policy, foreach_cell, Uout, smallr, smallp);
 
     timers.get("HydroUpdate_euler").stop();
   }
@@ -355,18 +457,17 @@ private:
   ForeachCell& foreach_cell;
   
   Timers& timers;  
+  Policy policy;
 
-  RiemannParams params;
-
-  BoundaryConditions bc_manager;
   GravityType gravity_type;
   real_t gx, gy, gz;
   bool well_balanced;
+  real_t smallr, smallp;
 };
 
 } // namespace dyablo
 
 FACTORY_REGISTER( dyablo::HydroUpdateFactory, 
-                  dyablo::HydroUpdate_euler_no_patch<dyablo::HydroState>, 
+                  dyablo::HydroUpdate_euler_no_patch<dyablo::FiniteVolumePolicy_hydro<3>>, 
                   "HydroUpdate_euler_no_patch")
 
