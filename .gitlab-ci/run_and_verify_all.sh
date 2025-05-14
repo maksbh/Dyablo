9a726e991d1748daf4e94332bacc0e1f0f86351a
Arnaud Durocher <arnaud.durocher@cea.fr>
2025-05-14 13:05:35 +0200

    Add zeldovitch_particles_dyablo CI test

--- .gitlab-ci/run_and_verify_all.sh
+++ .gitlab-ci/run_and_verify_all.sh
@@ -52,6 +52,7 @@ cd ../../..
 run_and_verify test_zeldovitch_grafic.ini "../../../settings/cosmo/validate_zeldovitch.py zeldovitch_main.xmf 0.2 ../../../zeldovitch_grafic/zeldovitch_grafic.png" zeldovitch_grafic 
 
 run_and_verify test_zeldovitch_dyablo.ini "../../../settings/cosmo/validate_zeldovitch.py zeldovitch_main.xmf 0.2 ../../../zeldovitch_dyablo/zeldovitch_dyablo.png" zeldovitch_dyablo 
+run_and_verify test_zeldovitch_particles_dyablo.ini "../../../settings/cosmo/validate_zeldovitch.py zeldovitch_main.xmf 0.2 ../../../zeldovitch_particles_dyablo/zeldovitch_particles_dyablo.png" zeldovitch_particles_dyablo 
 
 echo "${err_count}/${run_count} runs failed"
 
