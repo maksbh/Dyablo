ee016390be7b9097a8ad3b4b0d2c08faed3218a8
omarchal <olivier.marchal@astro.unistra.fr>
2025-07-18 08:43:23 +0200

    remove e_rad from ini file

--- core/src/init/InitialConditions_beam.cpp
+++ core/src/init/InitialConditions_beam.cpp
@@ -6,8 +6,6 @@ namespace dyablo{
 
     public:
 
-    using Photondens = decltype( Units::mol()/Units::code_units().getUnit<Units::Volume>() );
-
     InitialConditions_beam(
         ConfigMap& configMap, 
         ForeachCell& foreach_cell,  
@@ -18,16 +16,16 @@ namespace dyablo{
         if( !configMap.hasValue("cosmology","ctilde") )
         {
             real_t clight_fraction = configMap.getValue_in_unit( "cosmology", "clight_fraction", Units::one(), "10%");
-            real_t ctilde = clight_fraction * Units::constant_to_code_units( Units::SPEEDOFLIGHT() );
+            real_t astart = configMap.getValue<real_t>("cosmology", "astart", 1.0);
+            real_t ctilde = clight_fraction * Units::constant_to_code_units( Units::SPEEDOFLIGHT() ) * astart;
+
             configMap.getValue<real_t>("cosmology", "ctilde", ctilde);
         }
         
-        real_t e_rad_start = configMap.getValue_in_code_unit<Photondens>("rad", "e_rad_start", "1e-10 atom/m3");
-
         this->fields = { "rho","rho_HII","e_tot","rho_vx","rho_vy","rho_vz",
                         "e_rad","fx_rad","fy_rad","fz_rad"};
 
-        this->values = {0, 0, 0, 0, 0, 0, e_rad_start, 0, 0, 0};
+        this->values = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     }
 };
 
