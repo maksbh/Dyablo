db13cf8f199c19a6be6d60d82815ac17c22a423c
Arnaud Durocher <arnaud.durocher@cea.fr>
2025-12-19 17:02:08 +0100

    Refactor State_Ops using tuples to allow using arrays in States

--- core/src/states/State_Ops.h
+++ core/src/states/State_Ops.h
@@ -17,6 +17,83 @@
 
 namespace dyablo {
 
+
+namespace {
+
+template<int N>
+struct as_tuple_t
+{
+    template<typename S>
+    KOKKOS_INLINE_FUNCTION
+    static auto as_tuple(S& s)
+    {
+        static_assert( !std::is_same_v<S,S>, "as_tuple, not defined for this size" );
+    }
+};
+
+#define DEFINE_AS_TUPLE(N, ...) \
+template<> \
+struct as_tuple_t<N> \
+{ \
+    template<typename S> \
+    KOKKOS_INLINE_FUNCTION \
+    static auto as_tuple(S& s) \
+    { \
+        auto& [__VA_ARGS__] = s; \
+        return std::forward_as_tuple(__VA_ARGS__); \
+    } \
+};
+
+DEFINE_AS_TUPLE(1,e0)
+DEFINE_AS_TUPLE(2,e0,e1)
+DEFINE_AS_TUPLE(3,e0,e1,e2)
+DEFINE_AS_TUPLE(4,e0,e1,e2,e3)
+DEFINE_AS_TUPLE(5,e0,e1,e2,e3,e4)
+DEFINE_AS_TUPLE(6,e0,e1,e2,e3,e4,e5)
+DEFINE_AS_TUPLE(7,e0,e1,e2,e3,e4,e5,e6)
+DEFINE_AS_TUPLE(8,e0,e1,e2,e3,e4,e5,e6,e7)
+DEFINE_AS_TUPLE(9,e0,e1,e2,e3,e4,e5,e6,e7,e8)
+DEFINE_AS_TUPLE(10,e0,e1,e2,e3,e4,e5,e6,e7,e8,e9)
+DEFINE_AS_TUPLE(11,e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10)
+DEFINE_AS_TUPLE(12,e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11)
+DEFINE_AS_TUPLE(13,e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12)
+DEFINE_AS_TUPLE(14,e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13)
+DEFINE_AS_TUPLE(15,e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14)
+
+template <int I, typename T> 
+using state_tuple_elt_t = std::remove_reference_t<std::tuple_element_t<I,T>>;
+
+template< int I=0, typename F, typename... Tuple_t >
+KOKKOS_INLINE_FUNCTION
+void state_foreach_aux( const F& f, const Tuple_t&... t )
+{
+    constexpr size_t N = (std::tuple_size<Tuple_t>(),...);
+    static_assert( ((N == std::tuple_size<Tuple_t>()) && ...), "state_foreach : tuples not the same size" );
+
+    constexpr bool is_array = ((std::is_bounded_array_v<state_tuple_elt_t<I,Tuple_t>>) && ...);
+    static_assert( ((is_array == std::is_bounded_array_v<state_tuple_elt_t<I,Tuple_t>>) && ...), "state_foreach : tuples don't have the same arrays" );
+    if constexpr( is_array )
+    {
+        //constexpr size_t array_len = (std::size(state_tuple_elt_t<I,Tuple_t>{}),...);
+        constexpr size_t array_len = std::min({std::size(state_tuple_elt_t<I,Tuple_t>{}) ...});
+        static_assert( ((array_len == std::size(state_tuple_elt_t<I,Tuple_t>{})) && ...), "state_foreach : arrays not the same size" );
+        
+        for(int i=0; i<array_len; i++)
+        {
+            f( std::get<I>(t)[i]... );
+        }
+    }
+    else
+    {
+        f( std::get<I>(t)... );
+    }
+    
+    if constexpr( I+1 < N )
+        state_foreach_aux<I+1>(f, t...);
+}
+
+} // namespace
+
 /// By default T is not a State
 template<typename T>
 struct State_traits
@@ -24,55 +101,32 @@ struct State_traits
     static constexpr bool is_state = false;
 };
 
-template<int I>
-inline constexpr bool dependent_false_v = false;
-
-/// Type-trait for States
-#define DECLARE_STATE_TYPE_AUX( constness, State, N ) \
+#define DECLARE_STATE_TYPE_ARRAY_AUX(constness, State, N_VARS, N_FIELDS ) \
 template<> \
 struct State_traits<constness State> \
 { \
     static constexpr bool is_state = true; \
-    static constexpr int nvars = N; \
-    template< int I >  \
-    KOKKOS_INLINE_FUNCTION \
-    static constness real_t& get(constness State& s) \
+    static constexpr int nvars = N_VARS; \
+    KOKKOS_INLINE_FUNCTION\
+    static constness auto as_tuple( constness State& s ) \
     { \
-        static_assert(dependent_false_v<I>, "Missing variable in state `" #State "`, add it with DECLARE_STATE_GET()"); \
-        static real_t res = 0; \
-        return res; \
+        return as_tuple_t<N_FIELDS>::as_tuple(s); \
     } \
 }; \
 
-/// DECLARE_STATE_TYPE() declares `State` as a State and State_traits<State>::is_state == true;
-#define DECLARE_STATE_TYPE( State, N ) \
-DECLARE_STATE_TYPE_AUX( , State, N ); \
-DECLARE_STATE_TYPE_AUX( const , State, N ); \
+/// Type-trait for States without arrays
+#define DECLARE_STATE_TYPE( State, N_VARS) \
+DECLARE_STATE_TYPE_ARRAY_AUX(, State, N_VARS, N_VARS ) \
+DECLARE_STATE_TYPE_ARRAY_AUX(const, State, N_VARS, N_VARS ) \
 
-#define DECLARE_STATE_GET_AUX( State, I, expr ) \
-template<> KOKKOS_INLINE_FUNCTION real_t& State_traits<State>::get<I>( State& s ) { return expr; } \
-template<> KOKKOS_INLINE_FUNCTION const real_t& State_traits<const State>::get<I>( const State& s ) { return expr; } \
+/// Type-trait for States with arrays
+/// N_VARS is the total number of vars (summing array lengths)
+/// N_FIELDS are the number of fields in struct : (1 array = 1 field)
+#define DECLARE_STATE_TYPE_ARRAY( State, N_VARS, N_FIELDS ) \
+DECLARE_STATE_TYPE_ARRAY_AUX(, State, N_VARS, N_FIELDS ) \
+DECLARE_STATE_TYPE_ARRAY_AUX(const, State, N_VARS, N_FIELDS ) \
 
-/// Associates index I to field s.var (`State` Must be declared as a State with DECLARE_STATE_TYPE() before )
-#define DECLARE_STATE_GET( State, I, var ) \
-DECLARE_STATE_GET_AUX( State, I, s.var )
-
-/// Get field with index I form s (index associated with DECLARE_STATE_GET())
-template< int I, typename State_t >
-KOKKOS_INLINE_FUNCTION
-real_t& state_get( State_t& s )
-{
-    return State_traits<State_t>::template get<I>(s);
-}
-
-/// Get field with index I form s (index associated with DECLARE_STATE_GET()), const version
-template< int I, typename State_t >
-KOKKOS_INLINE_FUNCTION
-const real_t& state_get( const State_t& s )
-{
-    return State_traits<const State_t>::template get<I>(s);
-}
-
+#define DECLARE_STATE_GET( State, I, var ) /*empty*/
 
 /**
  * Iterate over each member variable for a set of states
@@ -85,15 +139,12 @@ const real_t& state_get( const State_t& s )
  *          one real& for each State& in `states`
  *          e.g. state_foreach_var( [](real_t&, real_t, real_t){...}, State&, const State&, const State& );
  **/
-template< int I=0, typename F, typename... State_t >
+template< typename F, typename... State_t >
 KOKKOS_INLINE_FUNCTION
 void state_foreach_var( const F& f, State_t&... states )
 {
-    f( state_get<I>(states)... );
-    if constexpr( ( (I+1 < State_traits<State_t>::nvars) && ...) )
-        state_foreach_var<I+1>(f, states...);
+    state_foreach_aux( f, State_traits<State_t>::as_tuple(states)... );
 }
-
 
 //################
 // Arithmetic operators on states
