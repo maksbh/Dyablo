bc9f2146db6fd0c1fd854b90738434fd21dbee7c
Maxime Delorme <maxime.delorme@cea.fr>
2026-01-26 15:03:28 +0100

    Thermal boundary conditions are managed inside the class

--- core/src/parabolic/ParabolicTerm_thermal_conduction.h
+++ core/src/parabolic/ParabolicTerm_thermal_conduction.h
@@ -9,6 +9,23 @@
 #include "utils/config/named_enum.h"
 
 /**
+ * Boundary conditions for thermal conduction
+ */
+enum ThermalBoundaryMode {
+  TBM_TEMPERATURE, 
+  TBM_TEMP_GRADIENT
+};
+
+template<>
+inline named_enum<ThermalBoundaryMode>::init_list named_enum<ThermalBoundaryMode>::names() 
+{
+  return {
+    {ThermalBoundaryMode::TBM_TEMPERATURE,   "temperature"},
+    {ThermalBoundaryMode::TBM_TEMP_GRADIENT, "temperature_gradient"}
+  };
+}
+
+/**
  * In the case we have an analytical kappa, this enum allows to pick which 
  * way kappa is calculated.
  */
@@ -59,7 +76,37 @@ public:
       ndim (configMap.getValue<int>("mesh", "ndim", 2) ),
       gamma0( configMap.getValue<real_t>("hydro","gamma0", 1.4) ),
       kappa_cst(configMap.getValue<real_t>("thermal_conduction", "kappa", 0.0)),
-      diffusivity_mode(configMap.getValue<DiffusivityMode>("thermal_conduction", "diffusivity_mode", DM_CONSTANT))
+      diffusivity_mode(configMap.getValue<DiffusivityMode>("thermal_conduction", "diffusivity_mode", DM_CONSTANT)),
+      bctc_min{
+        configMap.getValue<ThermalBoundaryMode>("thermal_conduction", "bctc_xmin", TBM_TEMP_GRADIENT),
+        configMap.getValue<ThermalBoundaryMode>("thermal_conduction", "bctc_ymin", TBM_TEMP_GRADIENT),
+        configMap.getValue<ThermalBoundaryMode>("thermal_conduction", "bctc_zmin", TBM_TEMP_GRADIENT)
+      },
+      bctc_max{
+        configMap.getValue<ThermalBoundaryMode>("thermal_conduction", "bctc_xmax", TBM_TEMP_GRADIENT),
+        configMap.getValue<ThermalBoundaryMode>("thermal_conduction", "bctc_ymax", TBM_TEMP_GRADIENT),
+        configMap.getValue<ThermalBoundaryMode>("thermal_conduction", "bctc_zmax", TBM_TEMP_GRADIENT)        
+      },
+      bctc_min_val{
+        configMap.getValue<real_t>("thermal_conduction", "bctc_xmin_val", 0.0),
+        configMap.getValue<real_t>("thermal_conduction", "bctc_ymin_val", 0.0),
+        configMap.getValue<real_t>("thermal_conduction", "bctc_zmin_val", 0.0)
+      },
+      bctc_max_val{
+        configMap.getValue<real_t>("thermal_conduction", "bctc_xmax_val", 0.0),
+        configMap.getValue<real_t>("thermal_conduction", "bctc_ymax_val", 0.0),
+        configMap.getValue<real_t>("thermal_conduction", "bctc_zmax_val", 0.0)        
+      },
+      min_pos {
+        configMap.getValue<real_t>("mesh", "xmin"),
+        configMap.getValue<real_t>("mesh", "ymin"),
+        configMap.getValue<real_t>("mesh", "zmin")
+      },
+      max_pos {
+        configMap.getValue<real_t>("mesh", "xmax"),
+        configMap.getValue<real_t>("mesh", "ymax"),
+        configMap.getValue<real_t>("mesh", "zmax")
+      }
       {
         if (diffusivity_mode == DM_ANALYTICAL) {
           kappa_mode = configMap.getValue<KappaMode>("thermal_conduction", "kappa_mode", KM_NONE);
@@ -76,6 +123,41 @@ public:
           kappa_mode = KM_NONE;
       };
 
+  /**
+   * @brief Computes the heat flux (Kappa grad T) at the boundary 
+   * @param q The primitive state in the first cell of the domain
+   * @param dh The size of the cell in the boundary direction
+   * @param dir The current axis
+   * @param pos The position of the boundary
+   * @param min_bound if the boundary is the "left" or "right" boundary
+   */
+  KOKKOS_INLINE_FUNCTION
+  real_t getBoundaryHeatFlux(const PrimHydroState   q,
+                             const real_t           dh,
+                             const ComponentIndex3D dir,
+                             const pos_t            pos,
+                             const bool             min_bound) const {
+    const real_t Tc = q.p / q.rho;
+    real_t kappa = kappa_cst;
+    real_t gradT = 0.0;
+
+    if (min_bound) {
+      kappa = 0.5 * (compute_kappa(pos, bctc_min_val[dir]) + compute_kappa(pos, Tc));
+      if (bctc_min[dir] == TBM_TEMPERATURE)
+        gradT = 2.0 * (Tc - bctc_min_val[dir]) / dh;
+      else if (bctc_min[dir] == TBM_TEMP_GRADIENT)
+        gradT = bctc_min_val[dir];
+    }
+    else {
+      kappa = 0.5 * (compute_kappa(pos, bctc_max_val[dir]) + compute_kappa(pos, Tc));
+      if (bctc_max[dir] == TBM_TEMPERATURE)
+        gradT = 2.0 * (bctc_max_val[dir] - Tc) / dh;
+      else if (bctc_max[dir] == TBM_TEMP_GRADIENT)
+        gradT = bctc_max_val[dir];
+    }
+
+    return kappa * gradT;
+  }
 
   KOKKOS_INLINE_FUNCTION
   real_t compute_kappa(const pos_t& pos, const real_t T) const {
@@ -241,10 +323,16 @@ public:
         FR += nfac * tmp_flux / (SR * size[dir]);
       }
 
-      if (iiL.is_boundary() && bc_manager.bc_min[dir] == BC_USER)
-        FL = bc_manager.overrideBoundaryHeatFlux<ndim, State>(FL, qC, kappaC, size[dir], dir, true);
-      if (iiR.is_boundary() && bc_manager.bc_max[dir] == BC_USER)
-        FR = bc_manager.overrideBoundaryHeatFlux<ndim, State>(FR, qC, kappaC, size[dir], dir, false);
+      if (iiL.is_boundary()) {
+        auto pos_bc = pos;
+        pos_bc[dir] = min_pos[dir];
+        FL = getBoundaryHeatFlux(qC, size[dir], dir, pos_bc, false);
+      }
+      if (iiR.is_boundary()) {
+        auto pos_bc = pos;
+        pos_bc[dir] = max_pos[dir];
+        FR = getBoundaryHeatFlux(qC, size[dir], dir, pos_bc, true);
+      }
 
       flux_out = area * (FR - FL);
 
@@ -270,6 +358,10 @@ private:
   real_t kappa_cst;
   DiffusivityMode diffusivity_mode;
   KappaMode kappa_mode;
+
+  ThermalBoundaryMode bctc_min[3], bctc_max[3];
+  real_t bctc_min_val[3], bctc_max_val[3];
+  real_t min_pos[3], max_pos[3];
 
   // Tri-Layer parameters
   real_t tr_thick, z1, z2, K1, K2;
