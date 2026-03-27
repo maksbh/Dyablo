02e930c2b9b6c55788fbe34de5e8941474bdbae6
Michel-Andres Breton <michel-andres.breton@obspm.fr>
2026-03-27 13:00:20 +0100

    Use functions from Binned_iOcts.hpp instead of local copy

--- core/src/foreach_cell/Binned_iOcts.hpp
+++ core/src/foreach_cell/Binned_iOcts.hpp
@@ -2,10 +2,69 @@
 
 #include <memory>
 
+#include "utils/misc/Dyablo_assert.h"
 #include "kokkos_shared.h"
 #include "amr/LightOctree_forward.h"
 
 namespace dyablo {
+
+struct Binned_iOcts{
+    Kokkos::View<uint32_t*> iOcts; /// All iOcts binned by level
+    std::vector<uint32_t> begin_v; /// first index for bin `level` in iOcts;
+};
+
+/***
+ * Sort iOcts to gather octants from the same bin together
+ * @param bin_max number of bins (gen_bin must return value between 0 and bin_max)
+ * @param nbOcts the number of octants to sort (gen_bin will be called from 0 to nbOcts)
+ * @param gen_bin is a uint32_t -> uint32_t function that returns the bin number for a given iOct
+ */
+template<typename F>
+Binned_iOcts bin_iOcts( int bin_max, int nbOcts, const F& gen_bin)
+{
+    int bin_count = bin_max + 1;
+    Kokkos::View<uint32_t*> bin_count_device("level_count",bin_count);
+    Kokkos::parallel_for( "bin_iOcts_count", nbOcts, 
+        KOKKOS_LAMBDA( uint32_t iOct )
+    {
+        int bin = gen_bin(iOct);
+        DYABLO_ASSERT_KOKKOS_DEBUG( 0 <= bin && bin <= bin_max, "bin_iOcts : gen_bin out of bounds" );
+
+        Kokkos::atomic_inc( &bin_count_device(bin) );
+    });
+
+    auto bin_count_host = Kokkos::create_mirror_view( bin_count_device );
+    Kokkos::deep_copy( bin_count_host, bin_count_device );
+
+    Kokkos::View<uint32_t*> iOcts("binned_iOcts", nbOcts);
+    std::vector<uint32_t> begin_v(bin_count+1);
+    uint32_t bin_begin = 0;
+    for( int bin = 0; bin <= bin_max; bin++ )
+    {
+        begin_v[bin] = bin_begin;
+        Kokkos::parallel_scan( "bin_iOcts_sort", nbOcts,
+            KOKKOS_LAMBDA( uint32_t iOct, int& i_local, bool final )
+        {
+            int oct_bin = gen_bin(iOct);
+            if( oct_bin == bin )
+            {
+                if( final )
+                   iOcts(bin_begin + i_local) = iOct; 
+                i_local++;
+            }
+        });
+        bin_begin += bin_count_host(bin);
+    }
+    begin_v[bin_max+1] = bin_begin;
+
+    return Binned_iOcts{
+        .iOcts = iOcts,
+        .begin_v = begin_v,
+    };
+}
+
+/// Get a subview from all iOcts in a given bin 
+Kokkos::View<uint32_t*> get_bin( const Binned_iOcts& iOcts, int bin );
 
 /**
  * Class to generate per-level octant lists
--- core/src/mpi/GhostCommunicator_Subset_levels.cpp
+++ core/src/mpi/GhostCommunicator_Subset_levels.cpp
@@ -11,74 +11,6 @@ struct Subset_levels::Pdata
     Binned_iOcts_levels binned_iOcts_levels;
 };
 
-} // namespace dyablo
-
-namespace {
-
-struct Binned_iOcts{
-    Kokkos::View<uint32_t*> iOcts; /// All iOcts binned by level
-    std::vector<uint32_t> begin_v; /// first index for bin `level` in iOcts;
-};
-
-/***
- * Sort iOcts to gather octants from the same bin together
- * @param bin_max number of bins (gen_bin must return value between 0 and bin_max)
- * @param nbOcts the number of octants to sort (gen_bin will be called from 0 to nbOcts)
- * @param gen_bin is a uint32_t -> uint32_t function that returns the bin number for a given iOct
- */
-template<typename F>
-Binned_iOcts bin_iOcts( int bin_max, int nbOcts, const F& gen_bin)
-{
-    int bin_count = bin_max + 1;
-    Kokkos::View<uint32_t*> bin_count_device("level_count",bin_count);
-    Kokkos::parallel_for( "bin_iOcts_count", nbOcts, 
-        KOKKOS_LAMBDA( uint32_t iOct )
-    {
-        int bin = gen_bin(iOct);
-        DYABLO_ASSERT_KOKKOS_DEBUG( 0 <= bin && bin <= bin_max, "bin_iOcts : gen_bin out of bounds" );
-
-        Kokkos::atomic_inc( &bin_count_device(bin) );
-    });
-
-    auto bin_count_host = Kokkos::create_mirror_view( bin_count_device );
-    Kokkos::deep_copy( bin_count_host, bin_count_device );
-
-    Kokkos::View<uint32_t*> iOcts("binned_iOcts", nbOcts);
-    std::vector<uint32_t> begin_v(bin_count+1);
-    uint32_t bin_begin = 0;
-    for( int bin = 0; bin <= bin_max; bin++ )
-    {
-        begin_v[bin] = bin_begin;
-        Kokkos::parallel_scan( "bin_iOcts_sort", nbOcts,
-            KOKKOS_LAMBDA( uint32_t iOct, int& i_local, bool final )
-        {
-            int oct_bin = gen_bin(iOct);
-            if( oct_bin == bin )
-            {
-                if( final )
-                   iOcts(bin_begin + i_local) = iOct; 
-                i_local++;
-            }
-        });
-        bin_begin += bin_count_host(bin);
-    }
-    begin_v[bin_max+1] = bin_begin;
-
-    return Binned_iOcts{
-        .iOcts = iOcts,
-        .begin_v = begin_v,
-    };
-}
-
-/// Get a subview from all iOcts in a given bin
-Kokkos::View<uint32_t*> get_bin( const Binned_iOcts& iOcts, int bin )
-{
-    DYABLO_ASSERT_HOST_DEBUG( bin+1 < iOcts.begin_v.size(), "Subset_levels::get_bin : bin out of bounds" );
-
-    return Kokkos::subview( iOcts.iOcts, std::make_pair( iOcts.begin_v.at(bin), iOcts.begin_v.at(bin+1) ) );
-}
-
-} // namespace
 
 namespace{
 
@@ -107,8 +39,6 @@ typename GhostComm_t::OctSubset getGhostCommunicatorSubset_level_impl(dyablo::Su
 }
     
 } //namespace
-
-namespace dyablo{
 
 GhostCommunicator_full_blocks::OctSubset Subset_levels::getGhostCommunicatorSubset_level( int level, const GhostCommunicator_full_blocks& comm )
 {
