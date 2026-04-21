f0f9094edc722941222421cb326ec6600dc71f70
Arnaud Durocher <arnaud.durocher@cea.fr>
2026-04-21 15:11:49 +0200

    Precompute only useful octants for LightOctree_hashmap_precompute

--- core/src/amr/LightOctree_hashmap_precompute.h
+++ core/src/amr/LightOctree_hashmap_precompute.h
@@ -20,6 +20,22 @@ static LightOctree_base::offset_t index_to_offset(uint32_t index, uint32_t ndims
 
     return LightOctree_base::offset_t{ (int8_t)(ix-1), (int8_t)(iy-1), (int8_t)((ndims==2)?0:iz-1)};
 }
+
+
+LightOctree_base::OctantIndex iOctLocal_to_OctantIndex(uint32_t iOct_local, uint32_t nbOcts)
+{
+    if( iOct_local < nbOcts )
+        return {.iOct=iOct_local, .isGhost=false, .isIntermediate=false};
+    else
+        return {.iOct=iOct_local-nbOcts, .isGhost=false, .isIntermediate=true};
+}
+
+uint32_t OctantIndex_to_iOctLocal( const LightOctree_base::OctantIndex& iOct, uint32_t nbOcts )
+{
+    return iOct.iOct + iOct.isIntermediate * nbOcts;
+}
+
+
 } // namespace
 
 inline void LightOctree_hashmap_precompute_init( 
@@ -30,35 +46,38 @@ inline void LightOctree_hashmap_precompute_init(
     uint32_t ndims )
 {
     uint32_t nneighbors = (ndims==2) ? 3*3 : 3*3*3;
-    uint32_t nbOcts_total = storage.getNumOctants()
-                          + storage.getNumGhosts()
-                          + storage.getNumIntermediates()
-                          + storage.getNumIntermediateGhosts();
-    neighbor_iOcts_leaves = Kokkos::View< uint32_t**, Kokkos::LayoutLeft >("LightOctree_hashmap_precompute::neighbor_iOcts", nbOcts_total, nneighbors);
-    neighbor_iOcts_intermediates = Kokkos::View< uint32_t**, Kokkos::LayoutLeft >("LightOctree_hashmap_precompute::neighbor_iOcts", nbOcts_total, nneighbors);
+    uint32_t nbOcts = storage.getNumOctants();
+    uint32_t nbIntermediates = storage.getNumIntermediates();
+    neighbor_iOcts_leaves = Kokkos::View< uint32_t**, Kokkos::LayoutLeft >("LightOctree_hashmap_precompute::neighbor_iOcts", nbOcts, nneighbors);
+    neighbor_iOcts_intermediates = Kokkos::View< uint32_t**, Kokkos::LayoutLeft >("LightOctree_hashmap_precompute::neighbor_iOcts", nbOcts+nbIntermediates, nneighbors);
 
-    Kokkos::parallel_for( "precompute_neighbors", nbOcts_total*nneighbors,
+    Kokkos::parallel_for( "precompute_neighbors_leaves", nbOcts*nneighbors,
+        KOKKOS_LAMBDA(uint32_t index)
+    {
+        uint32_t iOct_local = index%nbOcts;
+        uint32_t neighbor_id = index/nbOcts;
+
+        LightOctree_base::OctantIndex iOct = iOctLocal_to_OctantIndex(iOct_local, nbOcts);
+        LightOctree_base::offset_t offset = index_to_offset(neighbor_id, ndims);
+
+        LightOctree_base::OctantIndex iOct_neighbor_leaves = lmesh_hashmap.findNeighbor( iOct, offset );
+        uint32_t iOct_local_neighbor_leaves = OctantIndex_to_iOctLocal( iOct_neighbor_leaves, nbOcts );
+        neighbor_iOcts_leaves(iOct_local, neighbor_id) = iOct_local_neighbor_leaves;
+    });
+
+    uint32_t nbOcts_total = (nbOcts+nbIntermediates);
+    Kokkos::parallel_for( "precompute_neighbors_internediates", nbOcts_total*nneighbors,
         KOKKOS_LAMBDA(uint32_t index)
     {
         uint32_t iOct_local = index%nbOcts_total;
         uint32_t neighbor_id = index/nbOcts_total;
 
-        LightOctree_base::OctantIndex iOct = storage.iOctLocal_to_OctantIndex(iOct_local);
+        LightOctree_base::OctantIndex iOct = iOctLocal_to_OctantIndex(iOct_local, nbOcts);
         LightOctree_base::offset_t offset = index_to_offset(neighbor_id, ndims);
 
-        if( !(iOct.isGhost || iOct.isIntermediate) )
-        {
-            LightOctree_base::OctantIndex iOct_neighbor_leaves = lmesh_hashmap.findNeighbor( iOct, offset );
-            uint32_t iOct_local_neighbor_leaves = storage.OctantIndex_to_iOctLocal( iOct_neighbor_leaves );
-            neighbor_iOcts_leaves(iOct_local, neighbor_id) = iOct_local_neighbor_leaves;
-        }
-
-        if( !iOct.isGhost )
-        {
-            LightOctree_base::OctantIndex iOct_neighbor_intermediates = lmesh_hashmap.findNeighbor_intermediate( iOct, offset );
-            uint32_t iOct_local_neighbor_intermediates = storage.OctantIndex_to_iOctLocal( iOct_neighbor_intermediates );
-            neighbor_iOcts_intermediates(iOct_local, neighbor_id) = iOct_local_neighbor_intermediates;
-        }
+        LightOctree_base::OctantIndex iOct_neighbor_intermediates = lmesh_hashmap.findNeighbor_intermediate( iOct, offset );
+        uint32_t iOct_local_neighbor_intermediates = OctantIndex_to_iOctLocal( iOct_neighbor_intermediates, nbOcts );
+        neighbor_iOcts_intermediates(iOct_local, neighbor_id) = iOct_local_neighbor_intermediates;
     });
 }
 
@@ -88,11 +107,13 @@ public:
         if( iOct.isGhost )
             return LightOctree_hashmap::findNeighbor( iOct, offset ); //Ghosts are only on demand because we don't have all their neighbors.
 
-        uint32_t iOct_local = storage.OctantIndex_to_iOctLocal( iOct );
+        uint32_t nbOcts = storage.getNumOctants();
+
+        uint32_t iOct_local = OctantIndex_to_iOctLocal( iOct, nbOcts );
         uint32_t neighbor_id = offset_to_index(offset, getNdim());
         uint32_t iOct_local_neighbor = neighbor_iOcts( iOct_local, neighbor_id );
 
-        OctantIndex iOct_n = storage.iOctLocal_to_OctantIndex(iOct_local_neighbor);
+        OctantIndex iOct_n = iOctLocal_to_OctantIndex(iOct_local_neighbor, nbOcts);
         return iOct_n;
     }
 
